8ac09b944e4640bdd8984574720fa70794a53bbd
Matthew Kindt <matthewkindt96@yahoo.com>
2021-02-03 16:41:06 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,14 @@
+array_of_arrays = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+count = 0
+ 
+while count < array_of_arrays.length do
+  puts array_of_arrays[count][0]
+  puts array_of_arrays[count][1]
+  puts array_of_arrays[count][2]
+  count += 1
+end
